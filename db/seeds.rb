13c59e951a151e52912bae32f1494c68c9f86407
caroline faugas <cfaugas@hotmail.com>
2019-05-16 17:35:12 +0200

    controlleur

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+
+puts 'Creating cocktails...'
+cocktails_attributes = [
+  {
+    name:         'Mojito'
+  },
+  {
+    name:         'Mint Julep'
+  },
+  {
+    name:         'Whiskey Sour'
+  }
+]
+Cocktail.create!(cocktails_attributes)
+puts 'Cocktails!'
